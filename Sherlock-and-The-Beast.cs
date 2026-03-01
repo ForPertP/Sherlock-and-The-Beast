2ba73d8272ef83c1b94ea5dd50e064bc4928084f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-03-02 01:58:40 +0900

    Add decentNumber function to generate specific number
    Implemented the decentNumber function to generate a number consisting of fives and threes based on the given integer n.

--- Sherlock-and-The-Beast.cs
+++ Sherlock-and-The-Beast.cs
@@ -1,1 +1,39 @@
-Sherlock-and-The-Beast
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'decentNumber' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void decentNumber(int n)
+    {
+        for (int fives = n; fives >= 0; fives--)
+        {
+            if (fives % 3 == 0 && (n - fives) % 5 == 0)
+            {
+                Console.Write(new string('5', fives));
+                Console.Write(new string('3', n - fives));
+                Console.WriteLine();
+                return;
+            }
+        }
+
+        Console.WriteLine("-1");
+    }
+}
