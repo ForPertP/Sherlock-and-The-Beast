3ecbde598c08f589cbf091e98c962683ef9b9da7
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-03-03 01:55:33 +0900

    Implement decentNumber function in Sherlock-and-The-Beast
    Added imports and implemented decentNumber function to generate a number with specific properties.

--- Sherlock-and-The-Beast.java
+++ Sherlock-and-The-Beast.java
@@ -1,1 +1,40 @@
-Sherlock-and-The-Beast
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'decentNumber' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void decentNumber(int n) {
+        for (int fives = n; fives >= 0; fives--) {
+
+            if (fives % 3 == 0 && (n - fives) % 5 == 0) {
+
+                for (int i = 0; i < fives; i++)
+                    System.out.print("5");
+
+                for (int i = 0; i < n - fives; i++)
+                    System.out.print("3");
+
+                System.out.println();
+                return;
+            }
+        }
+
+        System.out.println("-1");
+    }
+}
+
